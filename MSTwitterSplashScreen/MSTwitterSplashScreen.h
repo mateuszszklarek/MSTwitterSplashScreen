8e01fa9a0401470d4f1c5da6bb760f2951e6d97f
Mateusz Szklarek <mateusz.szklarek@elpassion.pl>
2015-04-24 15:58:04 +0200

    Initial commit

---
+++ MSTwitterSplashScreen/MSTwitterSplashScreen.h
@@ -0,0 +1,13 @@
+//
+//  MSTwitterSplashScreen.h
+//  MSTwitterSplashScreen
+//
+//  Created by Mateusz Szklarek on 24/04/15.
+//  Copyright (c) 2015 Mateusz Szklarek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MSTwitterSplashScreen : NSObject
+
+@end
---
+++ MSTwitterSplashScreen/MSTwitterSplashScreen.m
@@ -0,0 +1,13 @@
+//
+//  MSTwitterSplashScreen.m
+//  MSTwitterSplashScreen
+//
+//  Created by Mateusz Szklarek on 24/04/15.
+//  Copyright (c) 2015 Mateusz Szklarek. All rights reserved.
+//
+
+#import "MSTwitterSplashScreen.h"
+
+@implementation MSTwitterSplashScreen
+
+@end
